9f7c0efa29133ad65c32e24c91f20b30a11f5ed2
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-13 09:11:28 -0400

    Created an operate function that will return a value based on the operator.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,23 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(operator, x, y) {
+  switch (operator) {
+    case '+':
+      return add(x, y);
+
+    case '-':
+      return subtract(x, y);
+
+    case '*':
+      return multiply(x, y);
+
+    case '/':
+      return divide(x, y);
+  }
+}
